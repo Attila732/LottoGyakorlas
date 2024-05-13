061365d2510a9cd5bb7a8e111502c05921374838
Attila732 <kovacsattila478@gmail.com>
2024-05-13 17:45:11 +0200

    Add project files.

---
+++ ClassLibrary/Data/LottoContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Data
+{
+    public class LottoContext : DbContext
+    {
+        public LottoContext() { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=APIHatosContext-a44052af-9303-4d93-bbaa-404cca2ac114;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+
+        public DbSet<ClassLibrary.Models.Jatekos> Jatekos { get; set; } = default!;
+        public DbSet<ClassLibrary.Models.Tipp> Tipp { get; set; } = default!;
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary.Data;
+
+LottoContext db = new LottoContext();
+
+
+if (!db.Jatekos.Any())
+{
+    var sorok = File.ReadAllLines(@"C:\Users\PC\Desktop\gyakorlás\Forras\csv\jatekosok.csv").Skip(1);
+    foreach (var line in sorok)
+        db.Jatekos.Add(new ClassLibrary.Models.Jatekos(line));
+    db.SaveChanges();   
+}
+if (!db.Tipp.Any())
+{
+    var sorok = File.ReadAllLines(@"C:\Users\PC\Desktop\gyakorlás\Forras\csv\tippek.csv").Skip(1);
+    foreach (var line in sorok)
+        db.Tipp.Add(new ClassLibrary.Models.Tipp(line));
+    db.SaveChanges();
+}
